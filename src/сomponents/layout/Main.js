ea0d24f69e3fba828de3d149201a054eb8a795f9
Freiz <freisentklod@gmail.com>
2021-03-15 18:54:25 +0600

    complete header

---
+++ src/сomponents/layout/Main.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components'
+
+import EmailsView from "./EmailsView";
+import Sidebar from "./Sidebar";
+import SideIcons from "./SideIcons";
+
+
+
+const Main = () => {
+    return (
+        <Wrapper>
+            <Sidebar />
+            <EmailsView />
+            <SideIcons />
+        </Wrapper>
+    )
+}
+
+export default Main;
+
+const Wrapper = styled.div `
+
+`
+
---
+++ src/сomponents/layout/SideIcons.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SideIcons = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SideIcons;
+
