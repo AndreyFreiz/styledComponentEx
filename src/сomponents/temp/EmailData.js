91eb4316e94a89d8f7621e371ebf5831a34de7d0
Freiz <freisentklod@gmail.com>
2021-03-17 18:37:56 +0600

    EmailSection and test data

---
+++ src/сomponents/temp/EmailData.js
@@ -0,0 +1,24 @@
+export const emailData = [
+    {
+        starred: true,
+        from: 'Test',
+        subject: 'Test',
+        message: 'Test',
+        received: '08:20',
+        read: true
+    }, {
+        starred: true,
+        from: 'Test2',
+        subject: 'Test',
+        message: 'Test Test',
+        received: '08:30',
+        read: true
+    }, {
+        starred: false,
+        from: 'Test3',
+        subject: 'Test',
+        message: 'Test Test',
+        received: '09:25',
+        read: false
+    },
+]
